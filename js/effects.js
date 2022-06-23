b0d442c8f685c4ac2f0df8754ab8a1f059724eb8
Esteban Pedraza <estemanps99@gmail.com>
2022-06-23 08:56:51 -0500

    first commit

---
+++ js/effects.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+
+    //Efecto menu
+    $('.menu a').each(function(index, elem){
+        $(this).css({
+            'top': '-200px'
+        });
+
+        $(this).animate({
+            'top': 0
+        }, 2000 + (index*500));
+    })
+
+
+    //Efecto header
+    if( $(window).width() > 800){
+        $('header .textos').css({
+            'opacity': '0',
+            'marginTop': '0'
+        });
+
+        $('header .textos').animate({
+            'opacity': '1',
+            'marginTop': '-52px'
+        }, 1500);
+    }
+
+    // Scroll elementos menu
+    var acercaDe = $('#acerca-de').offset().top,
+        menu = $('#platillos').offset().top,
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top;
+
+    $('#btn-acerca-de').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: acercaDe - 60
+        }, 500);
+    });
+    $('#btn-menu').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: menu
+        }, 500);
+    });
+    $('#btn-galeria').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: galeria
+        }, 500);
+    });
+    $('#btn-ubicacion').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: ubicacion
+        }, 500);
+    });
+
+    //Efecto resize
+    $(window).resize(function(){
+        var windowWidth = $(window).width();
+
+        if(windowWidth < 800){
+
+            $('header .textos').css({
+                'margin': '100px 0'
+            });
+        }
+    })
+});
